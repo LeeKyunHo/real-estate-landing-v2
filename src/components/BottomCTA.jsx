17ae085f2013f8ede020095da2949e61cf770a32
LKH <rbsgh369@gmail.com>
2026-04-16 16:29:31 +0900

    하단바 생성

---
+++ src/components/BottomCTA.jsx
@@ -0,0 +1,15 @@
+export default function BottomCTA() {
+  return (
+    <div className="bottom-cta">
+      <a href="tel:010-1234-5678" className="cta-btn call">
+        전화 상담
+      </a>
+      <a href="https://open.kakao.com/XXXX" className="cta-btn kakao">
+        카카오톡 상담
+      </a>
+      <a href="#contact" className="cta-btn reserve">
+        방문예약
+      </a>
+    </div>
+  );
+}
