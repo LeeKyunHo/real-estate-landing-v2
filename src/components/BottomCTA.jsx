5044b3b1abc119d743f18a7c623ecdac449e524b
kyunho <rbsgh369@gmail.com>
2026-04-17 23:33:37 +0900

    first commit

--- src/components/BottomCTA.jsx
+++ src/components/BottomCTA.jsx
@@ -4,9 +4,20 @@ export default function BottomCTA() {
       <a href="tel:010-1234-5678" className="cta-btn call">
         전화 상담
       </a>
+<<<<<<< HEAD
       <a href="https://open.kakao.com/XXXX" className="cta-btn kakao">
         카카오톡 상담
       </a>
+=======
+      <a 
+  href="https://open.kakao.com/o/soqX7s1h"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="cta-btn kakao"
+>
+  카카오톡 즉시 상담
+</a>
+>>>>>>> c406702 (first commit)
       <a href="#contact" className="cta-btn reserve">
         방문예약
       </a>
